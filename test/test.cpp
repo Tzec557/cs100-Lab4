f16942b38e01cb92565150dfcda1a15e9f0c6147
Tzec557 <ezhao012@ucr.edu>
2024-02-01 19:21:59 -0800

    update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -7,3 +7,33 @@ TEST(TriangleTests, testPerimeter) {
     Triangle *aTriangle = new Triangle(3,3,3);
     EXPECT_EQ (aTriangle->getPerimeter(),9);
 }
+
+TEST(TriangleTests, testPArea) {
+    Triangle *aTriangle = new Triangle(3,3,3);
+    EXPECT_EQ (aTriangle->getArea(),3.89711);
+}
+
+TEST(TriangleTest, testKindIsosceles) {
+    Kind result = triangle.getKind();
+    EXPECT_EQ(result, Kind::ISOSCELES);
+}
+
+TEST(TriangleTest, testKindEquilateral) {
+    Kind result = triangle.getKind();
+    EXPECT_EQ(result, Kind::EQUILATERAL);
+}
+
+TEST(TriangleTest, testKindScalene) {
+    Kind result = triangle.getKind();
+    EXPECT_EQ(result, Kind::SCALENE);
+}
+
+TEST(TriangleTests, testisIsosceles) {
+    Triangle *aTriangle = new Triangle(3,3,4);
+    EXPECT_TRUE (aTriangle->isIsosceles());
+}
+
+TEST(TriangleTests, testisIsosceles) {
+    Triangle *aTriangle = new Triangle(3,3,3);
+    EXPECT_TRUE (aTriangle->isEquilateral());
+}
