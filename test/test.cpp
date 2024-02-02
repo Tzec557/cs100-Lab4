7ef6de5dfc55411dc9fd6e517c17d3e31b611bb7
Tzec557 <ezhao012@ucr.edu>
2024-02-02 01:39:30 -0800

    update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,15 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "../include/Awards.h"  
 
-using awards::RankList;
-using awards::AwardCeremonyActions;
+using namespace awards;
+using ::testing::InSequence;
 
 class RankListStub : public awards::RankList {
 public:
     RankListStub() : currentIndex(0) {
-        names.push_back("Alice");
-        names.push_back("Bob");
+        names.push_back("Alex");
+        names.push_back("Brian");
         names.push_back("Charlie");
     }
 
@@ -22,35 +23,32 @@ public:
 
 private:
     std::vector<std::string> names;
-    size_t currentIndex;
+    int currentIndex;
 };
 
-#ifndef MOCK_AWARD_CEREMONY_ACTIONS_H
-#define MOCK_AWARD_CEREMONY_ACTIONS_H
 
-class MockAwardCeremonyActions : public awards::AwardCeremonyActions {
+class MockAwardCeremonyActions : public AwardCeremonyActions {
 public:
     MOCK_METHOD(void, playAnthem, (), (override));
-    MOCK_METHOD(void, awardBronze, (const std::string& name), (override));
-    MOCK_METHOD(void, awardSilver, (const std::string& name), (override));
-    MOCK_METHOD(void, awardGold, (const std::string& name), (override));
+    MOCK_METHOD(void, awardBronze, (std::string name), (override));
+    MOCK_METHOD(void, awardSilver, (std::string name), (override));
+    MOCK_METHOD(void, awardGold, (std::string name), (override));
     MOCK_METHOD(void, turnOffTheLightsAndGoHome, (), (override));
 };
-#endif
+
 
 TEST(AwardsTests, PerformAwardCeremonyCallsMethodsInCorrectOrder) {
-    
+
     RankListStub rankListStub;
     MockAwardCeremonyActions mockActions;
-    Awards awards(&mockActions, &rankListStub);
+    InSequence seq;
 
     EXPECT_CALL(mockActions, playAnthem()).Times(1);
-    EXPECT_CALL(mockActions, awardBronze("Alice")).Times(1);
-    EXPECT_CALL(mockActions, awardSilver("Bob")).Times(1);
+    EXPECT_CALL(mockActions, awardBronze("Alex")).Times(1);
+    EXPECT_CALL(mockActions, awardSilver("Brian")).Times(1);
     EXPECT_CALL(mockActions, awardGold("Charlie")).Times(1);
     EXPECT_CALL(mockActions, turnOffTheLightsAndGoHome()).Times(1);
 
-    
-    awards.performAwardCeremony();
+    performAwardCeremony(rankListStub, mockActions);
 
 }
